ce848768e4399ed6ed42aed9ae7683307d772845
Ely-ullen <eleanor.p.bradley@gmail.com>
2022-08-02 11:09:06 +0100

    completed task 2 4.GET

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,5 +1,14 @@
-const { selectTopics } = require("../models/models.js");
+const { selectTopics, selectArticle } = require("../models/models.js");
 
-exports.getTopics = (req, res, next) => {
+exports.getTopics = (req, res) => {
   selectTopics().then((topics) => res.status(200).send({ topics }));
+};
+
+exports.getArticle = (req, res, next) => {
+  const { article_id } = req.params;
+  selectArticle(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch(next);
 };
