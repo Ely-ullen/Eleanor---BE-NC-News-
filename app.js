3a1725e70b1546c8bac5457709122b8b119999f0
Ely-ullen <eleanor.p.bradley@gmail.com>
2022-08-22 10:09:06 +0100

    added cors

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+
 const {
   getTopics,
   patchVotes,
@@ -12,6 +14,9 @@ const {
 } = require("./controllers/controllers.js");
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 app.get("/api", getApis);
